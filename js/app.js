04b9e34c5b37903b7c38a13ad33be5a5f9cc8832
valentinburk <burkinman@me.com>
2018-03-12 01:31:01 +0300

    feat: Implement choosing pairs feature
    Every card marked with attribute 'data-card-id' for identification on choosing. Each id appears twice because there are pairs of cards. Correctly chosen pair becomes marked with attribute 'data-success'. If chosen pair is incorrect, this pair flips back to initial state.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,57 @@
+/**
+ * Logic
+ */
+
 $(document).ready(function() {
-  $('.flipper').click(function() {
-    $(this).addClass('flipper-open');
+  let openedCardId = null;
+  let ready = true;
+
+  $('.card').click(function() {
+    const currentId = flipCard($(this));
+
+    // if another card is open
+    if (openedCardId !== null) {
+      if (currentId === openedCardId) {
+        setSuccess(currentId);
+      } else {
+        flipWrongCards();
+      }
+      // reset
+      openedCardId = null;
+    } else {
+      openedCardId = currentId;
+    }
   });
 });
+
+/**
+ * Functions
+ */
+
+/**
+ * @description Flips the given card and returns its id
+ * @param {jQuery} card Card to flip
+ * @returns {number} Id of the flipped card
+ */
+function flipCard(card) {
+  card.find('.flipper').addClass('flipper-open');
+  return card.attr('data-card-id');
+}
+
+/**
+ * @description Sets the card to success state
+ * @param {number} cardId The id of the card
+ */
+function setSuccess(cardId) {
+  $(`div[data-card-id=${cardId}]`).attr('data-success', true);
+}
+
+/**
+ * @description Flips only wrong cards
+ */
+function flipWrongCards() {
+  setTimeout(function() {
+   $('.card:not([data-success])').find('.flipper')
+     .removeClass('flipper-open');
+  }, 500);
+}
